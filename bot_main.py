1c783fa254c253058e8c8d3e9b88e090e01f19c1
PortPointer32 <146490631+PortPointer32@users.noreply.github.com>
2023-12-17 11:26:37 +0200

    1

--- bot_main.py
+++ bot_main.py
@@ -1,11 +1,13 @@
+import requests
 import subprocess
+from datetime import datetime
 
 from aiogram.utils import executor
 from aiogram.dispatcher import Dispatcher
 
 from create_main_bot import dp
-from main_telegram_bot import Admin
 from cfg.database import Database
+from main_telegram_bot import Admin
 
 db = Database('/home/str/5arc/cfg/database')
 
